ad76a1d40e15a847e7f0b1b00b66d65f56184d57
Muhammad Taha <39196340+Tahawahid@users.noreply.github.com>
2024-03-20 10:22:14 +0500

    Add project files.

---
+++ MuhammadTaha_SP22_BSCS_0119/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MuhammadTaha_SP22_BSCS_0119.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
